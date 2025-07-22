60b4bb94a66889fec23bb3e936570b9034562c14
BartoszTochowicz <tochowiczbartosz@gmail.com>
2025-07-22 22:59:03 +0200

    skibidi

--- frontend/src/pages/Quiz.js
+++ frontend/src/pages/Quiz.js
@@ -5,27 +5,30 @@ import { useNavigate } from "react-router-dom";
 
 function Quiz() {
   const [questions, setQuestions] = useState([]);
-  const [idx, setIdx] = useState(0);
-  const [score, setScore] = useState(0);
-  const navigate = useNavigate();
+  const [answers, setAnswers] = useState([]);
+  const [token, setToken] = useState([]);
 
-  useEffect(() => {
-    axios.get("/api/quiz/")
-      .then(res => setQuestions(res.data))
-      .catch(() => alert("Error loading questions"));
-  }, []);
+  // const [idx, setIdx] = useState(0);
+  // const [score, setScore] = useState(0);
+  // const navigate = useNavigate();
 
-  function handleAnswer(ansKey) {
-    const isCorrect = questions[idx].correct_answers[`${ansKey}_correct`] === "true";
-    if (isCorrect) setScore(s => s + 1);
-    if (idx + 1 < questions.length) {
-      setIdx(i => i + 1);
-    } else {
-      navigate("/result", { state: { score, total: questions.length } });
-    }
-  }
+  // useEffect(() => {
+  //   axios.get("/quiz/singleplayer")
+  //     .then(res => setQuestions(res.data))
+  //     .catch(() => alert("Error loading questions"));
+  // }, []);
 
-  if (!questions.length) return <div>Loading...</div>;
+  // function handleAnswer(ansKey) {
+  //   const isCorrect = questions[idx].correct_answers[`${ansKey}_correct`] === "true";
+  //   if (isCorrect) setScore(s => s + 1);
+  //   if (idx + 1 < questions.length) {
+  //     setIdx(i => i + 1);
+  //   } else {
+  //     navigate("/result", { state: { score, total: questions.length } });
+  //   }
+  // }
+
+  // if (!questions.length) return <div>Loading...</div>;
 
   return (
     <div className="container">
